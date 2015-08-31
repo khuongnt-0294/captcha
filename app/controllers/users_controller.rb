2038e3269d792e1d542ea938750d5090a7b84843
KhuongNT <khuongnt1688@gmail.com>
2015-08-31 16:06:51 +0700

    use controller based

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     respond_to do |format|
-      if @user.save
+      if simple_captcha_valid? && @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
