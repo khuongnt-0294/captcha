aa5ea5b84fea1652bb3cc4f195bca367aec05a8d
KhuongNT <khuongnt1688@gmail.com>
2015-08-31 16:22:31 +0700

    use model based

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     respond_to do |format|
-      if simple_captcha_valid? && @user.save
+      if @user.valid_with_captcha? && @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email, :password)
+      params.require(:user).permit(:name, :email, :password, :captcha, :captcha_key)
     end
 end
